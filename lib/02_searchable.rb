90353be78568161089e3889000fe7125bf534d3c
Justin Anderson <janderson2k@gmail.com>
2016-09-05 15:53:28 -0700

    added 'where' keyword for queries

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,18 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    query_params = params.map do |param, value|
+      "#{param} = ?"
+    end.join(" AND ")
+    query = DBConnection.execute(<<-SQL, *params.values)
+      SELECT *
+      FROM #{self.table_name}
+      WHERE #{query_params}
+    SQL
+
+    parse_all(query)
   end
 end
 
-class SQLObject
-  # Mixin Searchable here...
+class SQLObject extend Searchable
 end
